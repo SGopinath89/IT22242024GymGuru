9368c5d95a36441468eea13ae2691c31bb002646
sharada417 <sharada.kanchana2001@gmail.com>
2025-03-04 10:57:31 +0530

    create navigation bar

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { Button } from "@/components/ui/button";
+import Greeting from "./Greeting";
+import { Switch } from "@/components/ui/switch"
+import Navigation from "./components/Navigation";
+
+
+const App = () => {
+  return (
+    <div>
+     <Navigation/>
+    </div>
+  );
+};
+
+export default App;
---
+++ frontend/src/components/Navigation.jsx
@@ -0,0 +1,57 @@
+import { Globe } from "lucide-react";
+import { Button } from "./ui/button";
+
+function Navigation(){
+    return(
+        <nav className="z-10 bg-green-900 flex item-center jusify-between px-8 text-white py-4">
+            <div className="flex items-center space-x-8">
+                <a href="/" className="text-2xl font-bold italic font-serif">
+                    Gym Guru
+                </a>
+            <div className="hidden md:flex space-x-6">
+                <a href={`/`} className="transition-colors">
+                    DashBoard
+                </a>
+            </div>
+            <div className="hidden md:flex space-x-6">
+                <a href={`/`} className="transition-colors">
+                    WorkOut
+                </a>
+            </div>
+            <div className="hidden md:flex space-x-6">
+                <a href={`/`} className="transition-colors">
+                    Tutorials
+                </a>
+            </div>
+            <div className="hidden md:flex space-x-6">
+                <a href={`/`} className="transition-colors">
+                    Blog
+                </a>
+            </div>
+            <div className="hidden md:flex space-x-6">
+                <a href={`/`} className="transition-colors">
+                    Challenge
+                </a>
+            </div>
+            <div className="hidden md:flex space-x-6">
+                <a href={`/`} className="transition-colors">
+                    Contact
+                </a>
+            </div>
+            </div>
+            <div className="flex items-center space-x-4 ml-auto">
+                <Button varient="ghost" asChild>
+                    <a href="/sign-in">Login</a>
+                </Button>
+        
+                <Button varient="ghost" asChild>
+                    <a href="/sign-up">Sign up</a>
+                </Button>
+                <div>
+                    <p>sharada</p>
+                </div>
+            </div>
+        </nav>
+    );
+}
+export default Navigation;
